977f44bd772a3df8760f1ae14be8f8ed4424ce81
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-04-05 20:38:15 +0600

    Updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const openView = document.getElementById('open-view');
-const closeView = document.getElementById('close-view');
-const flipCard = document.getElementById('flipCard');
-openView.addEventListener('click',viewModeChange1);
-closeView.addEventListener('click',viewModeChange2);
+// const openView = document.getElementById('open-view');
+// const closeView = document.getElementById('close-view');
+// const flipCard = document.getElementById('flipCard');
+// openView.addEventListener('click',viewModeChange1);
+// closeView.addEventListener('click',viewModeChange2);
 
 function viewModeChange1(){
     flipCard.style.transform = "rotateY(180deg)";
